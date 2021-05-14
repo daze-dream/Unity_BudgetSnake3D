48d29f32e84dc2f1f1f3a617eb43f4fdf2a95408
CheetoDustNeo <misterrolypoly@gmail.com>
2021-05-14 02:13:58 -0500

    tweaked enemy speeds

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -23,19 +23,19 @@ public class EnemySpawnManager : MonoBehaviour
         switch (difficulty)
         {
             case 1:
-                Instantiate(enemyPrefab, new Vector3(0, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8, 8), 0, 9);
+                Instantiate(enemyPrefab, new Vector3(0, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
                 break;
             case 2:
-                Instantiate(enemyPrefab, new Vector3(-14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-15, 15), 0, 9);
-                Instantiate(enemyPrefab, new Vector3(14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-15, 15), 0, 9);
+                Instantiate(enemyPrefab, new Vector3(-14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
+                Instantiate(enemyPrefab, new Vector3(14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
                 break;
             case 3:
-                Instantiate(enemyPrefab, new Vector3(-14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-15, 15), 0, 9);
-                Instantiate(enemyPrefab, new Vector3(0, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8, 8), 0, 9);
-                Instantiate(enemyPrefab, new Vector3(14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-15, 15), 0, 9);
+                Instantiate(enemyPrefab, new Vector3(-14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
+                Instantiate(enemyPrefab, new Vector3(0, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
+                Instantiate(enemyPrefab, new Vector3(14, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
                 break;
             default:
-                Instantiate(enemyPrefab, new Vector3(0, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-8, 8), 0, 14);
+                Instantiate(enemyPrefab, new Vector3(0, 1, -0.2f), enemyPrefab.transform.rotation).GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-4, 4), 0, 9);
                 break;
         }
 
