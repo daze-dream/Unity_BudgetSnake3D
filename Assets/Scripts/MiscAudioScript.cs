fc81e6330be56bd7846039f3ef74c95d2052cdae
CheetoDustNeo <misterrolypoly@gmail.com>
2021-05-16 01:31:08 -0500

    Gold Bugfix2: numerous fixes, see desc
    - fixed multiple audio object spawning between scene changes due to DontDestroyOnLoad: see MiscAudioScript
    - added additional tags

--- Assets/Scripts/MiscAudioScript.cs
+++ Assets/Scripts/MiscAudioScript.cs
@@ -8,7 +8,11 @@ public class MiscAudioScript : MonoBehaviour
     public AudioClip allDeathSound;
     public AudioClip nibbleDeathSound;
     public AudioSource thisAudio;
+    //these variables are to prevent more and more of the parent object to be spawned
+    private GameObject[] other;
+    bool NOTFIRST = false;
     // Start is called before the first frame update
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
@@ -18,7 +22,34 @@ public class MiscAudioScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    /// <summary>
+    /// this is to prevent multiple parent objs spawning on scene load. Deletes the old one.
+    /// </summary>
+    private void Awake()
+    {
+        //find all objects in the scene of that tag
+        other = GameObject.FindGameObjectsWithTag("Misc Music");
+        //iterate through
+        foreach (GameObject oneOther in other)
+        {
+            //if this object is not the first one in the build index...
+            if (oneOther.scene.buildIndex == -1)
+            {
+                //means its the old one
+                NOTFIRST = true;
+            }
+        }
+        //so destroy it
+        if (NOTFIRST == true)
+        {
+            Destroy(gameObject);
+        }
+        //posterity code
+        DontDestroyOnLoad(gameObject);
+        thisAudio = GetComponent<AudioSource>();
     }
 
     public void PlayMenuSound()
